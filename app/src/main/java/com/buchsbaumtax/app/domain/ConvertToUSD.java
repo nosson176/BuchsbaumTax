431fad9b6a23b49e89d7b8094e913f07bb9c57aa
Shalom Vanderhoof <44070783+thesabermaniac@users.noreply.github.com>
2021-11-21 10:11:18 +0200

    feedback

--- app/src/main/java/com/buchsbaumtax/app/domain/ConvertToUSD.java
+++ app/src/main/java/com/buchsbaumtax/app/domain/ConvertToUSD.java
@@ -4,13 +4,17 @@ import com.buchsbaumtax.core.dao.ExchangeRateDAO;
 import com.buchsbaumtax.core.model.ExchangeRate;
 import com.sifradigital.framework.db.Database;
 
+import java.util.List;
+
 public class ConvertToUSD {
+    private static final List<ExchangeRate> exchangeRates = Database.dao(ExchangeRateDAO.class).getAll();
+
     public static Double convertToUSD(double amount, String currency, String year) {
         if (currency != null && year != null) {
             if (currency.equals("USD")) {
                 return amount;
             }
-            ExchangeRate exchangeRate = Database.dao(ExchangeRateDAO.class).getForCurrencyYear(currency, year);
+            ExchangeRate exchangeRate = exchangeRates.stream().filter(er -> (er.getYear() != null && er.getYear().equals(year)) && (er.getCurrency() != null && er.getCurrency().equals(currency))).findFirst().orElse(null);
             if (exchangeRate != null) {
                 return amount / exchangeRate.getRate();
             }
--- app/src/main/java/com/buchsbaumtax/core/dao/ExchangeRateDAO.java
+++ app/src/main/java/com/buchsbaumtax/core/dao/ExchangeRateDAO.java
@@ -3,12 +3,13 @@ package com.buchsbaumtax.core.dao;
 import com.buchsbaumtax.core.model.ExchangeRate;
 import com.sifradigital.framework.db.Dao;
 import org.jdbi.v3.sqlobject.config.RegisterFieldMapper;
-import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+
+import java.util.List;
 
 @Dao
 public interface ExchangeRateDAO {
     @RegisterFieldMapper(ExchangeRate.class)
-    @SqlQuery("SELECT * FROM exchange_rates WHERE currency = :currency AND year = :year")
-    ExchangeRate getForCurrencyYear(@Bind("currency") String currency, @Bind("year") String year);
+    @SqlQuery("SELECT * FROM exchange_rates ORDER BY id")
+    List<ExchangeRate> getAll();
 }
