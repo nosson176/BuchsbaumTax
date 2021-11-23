49d4e1e6169082893e5c80beee3b74a720a2d1cb
Shalom Vanderhoof <44070783+thesabermaniac@users.noreply.github.com>
2021-11-23 12:55:20 +0200

    client history

--- app/src/main/java/com/buchsbaumtax/app/domain/taxyear/GetClientData.java
+++ app/src/main/java/com/buchsbaumtax/app/domain/taxyear/GetClientData.java
@@ -6,11 +6,7 @@ import com.buchsbaumtax.app.dto.TaxYearData;
 import com.buchsbaumtax.core.dao.ClientDAO;
 import com.buchsbaumtax.core.dao.ClientHistoryDAO;
 import com.buchsbaumtax.core.dao.TaxYearDAO;
-import com.buchsbaumtax.core.model.Client;
-import com.buchsbaumtax.core.model.FbarBreakdown;
-import com.buchsbaumtax.core.model.IncomeBreakdown;
-import com.buchsbaumtax.core.model.TaxYear;
-import com.buchsbaumtax.core.model.User;
+import com.buchsbaumtax.core.model.*;
 import com.sifradigital.framework.db.Database;
 
 import java.util.List;
@@ -22,6 +18,9 @@ public class GetClientData {
         Client client = Database.dao(ClientDAO.class).get(clientId);
         List<TaxYear> taxYears = Database.dao(TaxYearDAO.class).getByClient(client.getId());
         List<TaxYearData> taxYearData = taxYears.stream().map(TaxYearData::new).collect(Collectors.toList());
+        
+        Database.dao(ClientHistoryDAO.class).create(user.getId(), clientId);
+
         ClientData clientData = new ClientData(client, taxYearData);
         List<IncomeBreakdown> incomeBreakdowns = clientData.getIncomeBreakdowns();
         for (IncomeBreakdown breakdown : incomeBreakdowns) {
