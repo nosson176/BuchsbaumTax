39a7cbf9d75a3d695a2bdddaf0a28f9b937ec003
Shalom Vanderhoof <44070783+thesabermaniac@users.noreply.github.com>
2021-11-23 10:27:30 +0200

    feedback

--- app/src/main/java/com/buchsbaumtax/app/domain/ConvertToUSD.java
+++ app/src/main/java/com/buchsbaumtax/app/domain/ConvertToUSD.java
@@ -1,5 +1,6 @@
 package com.buchsbaumtax.app.domain;
 
+import com.buchsbaumtax.app.dto.ExchangeRateObject;
 import com.buchsbaumtax.core.dao.ExchangeRateDAO;
 import com.buchsbaumtax.core.model.ExchangeRate;
 import com.github.benmanes.caffeine.cache.Cache;
@@ -7,25 +8,26 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import com.sifradigital.framework.db.Database;
 
 public class ConvertToUSD {
-    static Cache<String, Double> cache = Caffeine.newBuilder().build();
+    static Cache<ExchangeRateObject, Double> cache = Caffeine.newBuilder().build();
 
     public static Double convertToUSD(double amount, String currency, String year) {
         if (currency != null && year != null) {
             if (currency.equals("USD")) {
                 return amount;
             }
-            String key = currency + year;
-            Double rate = cache.getIfPresent(key);
-            if (rate == null) {
-                ExchangeRate exchangeRate = Database.dao(ExchangeRateDAO.class).getForCurrencyYear(currency, year);
-                if (exchangeRate != null) {
-                    cache.put(key, exchangeRate.getRate());
-                    rate = cache.getIfPresent(key);
-                }
-            }
+            ExchangeRateObject key = new ExchangeRateObject(year, currency);
+            Double rate = cache.get(key, k -> getRate(year, currency));
             if (rate != null) {
                 return amount / rate;
             }
+        }
+        return null;
+    }
+
+    private static Double getRate(String year, String currency) {
+        ExchangeRate exchangeRate = Database.dao(ExchangeRateDAO.class).getForCurrencyYear(currency, year);
+        if (exchangeRate != null) {
+            return exchangeRate.getRate();
         }
         return null;
     }
