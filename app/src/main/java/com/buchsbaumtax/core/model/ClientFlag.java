d9f32d5ada9380adea7cb7b8629ed53d38afde61
Shalom Vanderhoof <44070783+thesabermaniac@users.noreply.github.com>
2021-12-06 15:53:00 +0200

    added ClientFlag class

---
+++ app/src/main/java/com/buchsbaumtax/core/model/ClientFlag.java
@@ -0,0 +1,27 @@
+package com.buchsbaumtax.core.model;
+
+
+public class ClientFlag {
+
+    private long id;
+    private long clientId;
+    private long userId;
+    private long flag;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public long getFlag() {
+        return flag;
+    }
+}
