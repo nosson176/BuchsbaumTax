c5ee7deb2ba6dbec615ba9faa124d7fbb6fb4ee2
Yehoshua Levine <shuielevine@gmail.com>
2022-08-04 15:02:00 +0300

    Refactor sort order

--- app/src/main/java/com/buchsbaumtax/app/domain/UpdateValue.java
+++ app/src/main/java/com/buchsbaumtax/app/domain/UpdateValue.java
@@ -18,29 +18,33 @@ public class UpdateValue {
         Value old = Database.dao(ValueDAO.class).get(valueId);
         if (old.getSortOrder() != value.getSortOrder()) {
             List<Value> values = Database.dao(ValueDAO.class).getByKey(old.getKey());
-
-            // force current order
-            for (int i = 0; i < values.size(); i++) {
-                values.get(i).setSortOrder(i + 1);
-            }
-
-            // reorder
-            boolean movedUp = old.getSortOrder() > value.getSortOrder();
-            for (Value v : values) {
-                if (movedUp) {
-                    if (v.getSortOrder() >= value.getSortOrder() && v.getSortOrder() < old.getSortOrder()) {
-                        v.setSortOrder(v.getSortOrder() + 1);
-                    }
-                }
-                else {
-                    if (v.getSortOrder() > old.getSortOrder() && v.getSortOrder() <= value.getSortOrder()) {
-                        v.setSortOrder(v.getSortOrder() - 1);
-                    }
-                }
-            }
-            Database.dao(ValueDAO.class).update(values);
+            reorder(values, old.getSortOrder(), value.getSortOrder());
         }
         Database.dao(ValueDAO.class).update(value);
         return new ValueObject(Database.dao(ValueDAO.class).get(value.getId()));
+    }
+
+    private void reorder(List<Value> values, int oldSort, int newSort) {
+
+        // force current order
+        for (int i = 0; i < values.size(); i++) {
+            values.get(i).setSortOrder(i + 1);
+        }
+
+        // reorder
+        boolean movedUp = oldSort > newSort;
+        for (Value v : values) {
+            if (movedUp) {
+                if (v.getSortOrder() >= newSort && v.getSortOrder() < oldSort) {
+                    v.setSortOrder(v.getSortOrder() + 1);
+                }
+            }
+            else {
+                if (v.getSortOrder() > oldSort && v.getSortOrder() <= newSort) {
+                    v.setSortOrder(v.getSortOrder() - 1);
+                }
+            }
+        }
+        Database.dao(ValueDAO.class).update(values);
     }
 }
