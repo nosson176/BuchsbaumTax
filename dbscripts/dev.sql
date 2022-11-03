8c5dcb377d638fa64b3dc710f615fcd12d49a7f7
Yehoshua Levine <shuielevine@gmail.com>
2022-11-03 13:43:21 +0200

    Dev env variables

---
+++ dbscripts/dev.sql
@@ -0,0 +1,5 @@
+INSERT INTO environment_properties (property_key, property_value) VALUES ('twilio-account-sid', 'AC085231703d31d2be7725bb28a86d7ae7');
+INSERT INTO environment_properties (property_key, property_value) VALUES ('twilio-auth-token', '200ee5f554fe8db16e415837a20969a8');
+INSERT INTO environment_properties (property_key, property_value) VALUES ('twilio-from-number', '+13167452842');
+INSERT INTO phone_numbers (phone_number, name) VALUES ('+972544331975', 'NOSSON');
+INSERT INTO phone_numbers (phone_number, name) VALUES ('+972547922856', 'NOSSON');
