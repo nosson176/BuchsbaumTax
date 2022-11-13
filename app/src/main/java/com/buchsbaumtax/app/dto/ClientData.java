4ce27953e00720f00f7316be81fca805a781ccf4
Yehoshua Levine <shuielevine@gmail.com>
2022-11-13 12:28:19 +0200

    Data for calculation

--- app/src/main/java/com/buchsbaumtax/app/dto/ClientData.java
+++ app/src/main/java/com/buchsbaumtax/app/dto/ClientData.java
@@ -6,37 +6,39 @@ import com.sifradigital.framework.db.Database;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ClientData {
+
     public static final String DOLLARS = "USD";
     public static final String SHEKELS = "NIS";
 
-    private int id;
-    private String lastName;
-    private String status;
-    private String owesStatus;
-    private boolean archived;
-    private String periodical;
-    private String displayName;
-    private String displayPhone;
-    private Date created;
-    private List<TaxYearData> taxYearData;
-    private List<FbarBreakdown> fbarBreakdowns;
-    private List<IncomeBreakdown> incomeBreakdowns;
-    private List<Log> logs;
-    private List<Contact> contacts;
-    private List<TaxPersonal> taxPersonals;
-    private List<Fee> fees;
-    private List<Checklist> checklists;
-    private Double owesDollars;
-    private Double paidDollars;
-    private Double owesShekels;
-    private Double paidShekels;
-    private Double feesOwesDollars;
-    private Double feesPaidDollars;
-    private Double feesOwesShekels;
-    private Double feesPaidShekels;
+    private final int id;
+    private final String lastName;
+    private final String status;
+    private final String owesStatus;
+    private final boolean archived;
+    private final String periodical;
+    private final String displayName;
+    private final String displayPhone;
+    private final Date created;
+    private final List<TaxYearData> taxYearData;
+    private final List<FbarBreakdown> fbarBreakdowns;
+    private final List<IncomeBreakdown> incomeBreakdowns;
+    private final List<Log> logs;
+    private final List<Contact> contacts;
+    private final List<TaxPersonal> taxPersonals;
+    private final List<Fee> fees;
+    private final List<Checklist> checklists;
+    private final double owesDollars;
+    private final double paidDollars;
+    private final double owesShekels;
+    private final double paidShekels;
+    private final double feesOwesDollars;
+    private final double feesPaidDollars;
+    private final double feesOwesShekels;
+    private final double feesPaidShekels;
 
 
     public ClientData(Client client, List<TaxYearData> taxYearData) {
@@ -58,38 +60,37 @@ public class ClientData {
         this.fees = Database.dao(FeeDAO.class).getForClient(client.getId());
         this.checklists = Database.dao(ChecklistDAO.class).getForClient(client.getId());
 
-        Stream<Filing> clientFilings = Database.dao(FilingDAO.class).getByClient(client.getId()).stream().filter(Filing::isIncludeInRefund);
-        this.owesDollars = Database.dao(FilingDAO.class).getByClient(client.getId()).stream()
+        List<Filing> filings = taxYearData.stream().flatMap(t -> t.getFilings().stream()).collect(Collectors.toList());
+        this.owesDollars = filings.stream()
                 .filter(Filing::isIncludeInRefund)
                 .filter(f -> f.getCurrency() != null && f.getCurrency().equals(DOLLARS))
                 .mapToDouble(f -> f.getOwes() + f.getOwesFee()).sum();
-        this.paidDollars = Database.dao(FilingDAO.class).getByClient(client.getId()).stream()
+        this.paidDollars = filings.stream()
                 .filter(Filing::isIncludeInRefund)
                 .filter(f -> f.getCurrency() != null && f.getCurrency().equals(DOLLARS))
                 .mapToDouble(f -> f.getPaid() + f.getPaidFee()).sum();
-        this.owesShekels = Database.dao(FilingDAO.class).getByClient(client.getId()).stream()
+        this.owesShekels = filings.stream()
                 .filter(Filing::isIncludeInRefund)
                 .filter(f -> f.getCurrency() != null && f.getCurrency().equals(SHEKELS))
                 .mapToDouble(f -> f.getOwes() + f.getOwesFee()).sum();
-        this.paidShekels = Database.dao(FilingDAO.class).getByClient(client.getId()).stream()
+        this.paidShekels = filings.stream()
                 .filter(Filing::isIncludeInRefund)
                 .filter(f -> f.getCurrency() != null && f.getCurrency().equals(SHEKELS))
                 .mapToDouble(f -> f.getPaid() + f.getPaidFee()).sum();
 
-        Stream<Fee> clientFees = Database.dao(FeeDAO.class).getForClient(client.getId()).stream().filter(Fee::isInclude);
-        this.feesOwesDollars = Database.dao(FeeDAO.class).getForClient(client.getId()).stream()
+        this.feesOwesDollars =fees.stream()
                 .filter(Fee::isInclude)
                 .filter(f -> f.getCurrency() != null && f.getCurrency().equals(DOLLARS))
                 .mapToDouble(Fee::getManualAmount).sum();
-        this.feesPaidDollars = Database.dao(FeeDAO.class).getForClient(client.getId()).stream()
+        this.feesPaidDollars = fees.stream()
                 .filter(Fee::isInclude)
                 .filter(f -> f.getCurrency() != null && f.getCurrency().equals(DOLLARS))
                 .mapToDouble(Fee::getPaidAmount).sum();
-        this.feesOwesShekels = Database.dao(FeeDAO.class).getForClient(client.getId()).stream()
+        this.feesOwesShekels = fees.stream()
                 .filter(Fee::isInclude)
                 .filter(f -> f.getCurrency() != null && f.getCurrency().equals(SHEKELS))
                 .mapToDouble(Fee::getManualAmount).sum();
-        this.feesPaidShekels = Database.dao(FeeDAO.class).getForClient(client.getId()).stream()
+        this.feesPaidShekels = fees.stream()
                 .filter(Fee::isInclude)
                 .filter(f -> f.getCurrency() != null && f.getCurrency().equals(SHEKELS))
                 .mapToDouble(Fee::getPaidAmount).sum();
