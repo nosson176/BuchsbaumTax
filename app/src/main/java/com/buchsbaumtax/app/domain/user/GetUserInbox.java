cd0b6bc5623d05cc82767f64774d50a5cfd9d0cb
Yehoshua Levine <shuielevine@gmail.com>
2022-11-21 11:21:42 +0200

    Thread logic

--- app/src/main/java/com/buchsbaumtax/app/domain/user/GetUserInbox.java
+++ app/src/main/java/com/buchsbaumtax/app/domain/user/GetUserInbox.java
@@ -3,18 +3,28 @@ package com.buchsbaumtax.app.domain.user;
 import com.buchsbaumtax.app.dto.UserMessageObject;
 import com.buchsbaumtax.core.dao.UserMessageDAO;
 import com.buchsbaumtax.core.model.User;
+import com.buchsbaumtax.core.model.UserMessage;
 import com.sifradigital.framework.db.Database;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GetUserInbox {
 
     public List<UserMessageObject> getUserInbox(User user) {
-        return Database.dao(UserMessageDAO.class).getByUser(user.getId())
-                .stream()
-                .filter(m -> m.getParentId() == null)
-                .map(UserMessageObject::new)
-                .collect(Collectors.toList());
+        List<UserMessage> messages = Database.dao(UserMessageDAO.class).getByUser(user.getId());
+
+        List<UserMessageObject> results = new ArrayList<>();
+        for (UserMessage message : messages) {
+            if (message.getThreadId() != null) {
+                if (results.stream().noneMatch(m -> m.getId() == message.getThreadId())) {
+                    results.add(new UserMessageObject(Database.dao(UserMessageDAO.class).get(message.getThreadId())));
+                }
+            }
+            else {
+                results.add(new UserMessageObject(message));
+            }
+        }
+       return results;
     }
 }
